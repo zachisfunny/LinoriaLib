bf3e23261f4e136136ac29b8c018b7c709d5725f
zachisfunny <105691238+zachisfunny@users.noreply.github.com>
2022-08-17 10:42:35 -0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1074,7 +1074,7 @@ do
             BackgroundColor3 = Library.MainColor;
             BorderColor3 = Library.OutlineColor;
             BorderMode = Enum.BorderMode.Inset;
-            Size = UDim2.new(0.5, 0, 0.5, 0);
+            Size = UDim2.new(1, 0, 1, 0);
             ZIndex = 6;
             Parent = ButtonOuter;
         });
