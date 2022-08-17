708aa35b2a48f3451e077904328974426cf2b1a1
zachisfunny <105691238+zachisfunny@users.noreply.github.com>
2022-08-17 11:41:07 -0700

    Update Settings.lua

--- Extras/Settings.lua
+++ Extras/Settings.lua
@@ -1,1 +1,35 @@
+--Settings | Add [ UI Settings = Window:AddTab('UI Settings'), ] In your tabs for a UI Settings tab
+Library:OnUnload(function()
+    print('Unloaded!')
+    Library.Unloaded = true
+end)
 
+local MenuGroup = Tabs.UI Settings:AddLeftGroupbox('Menu')
+
+MenuGroup:AddButton('Unload', function() Library:Unload() end)
+MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = false, Text = 'Menu keybind' }) 
+MenuGroup:AddToggle('kbframevisible', {
+    Text = 'Show Keybinds',
+    Default = true,
+    Tooltip = 'Shows your Keybinds',
+})
+
+Toggles.kbframevisible:OnChanged(function()
+Library.KeybindFrame.Visible = Toggles.kbframevisible.Value
+end)
+
+Library.ToggleKeybind = Options.MenuKeybind -- Allows you to have a custom keybind for the menu
+
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+
+SaveManager:IgnoreThemeSettings() 
+
+SaveManager:SetIgnoreIndexes({ 'MenuKeybind' }) 
+
+ThemeManager:SetFolder('foldername')
+SaveManager:SetFolder('foldername')
+
+SaveManager:BuildConfigSection(Tabs.UI Settings) 
+
+ThemeManager:ApplyToTab(Tabs.UI Settings)
