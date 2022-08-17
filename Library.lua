72199c16f4fcfe327886be11678fe8994823687d
zachisfunny <105691238+zachisfunny@users.noreply.github.com>
2022-08-17 10:39:09 -0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1061,7 +1061,7 @@ do
 
         local ButtonOuter = Library:Create('Frame', {
             BorderColor3 = Color3.new(0, 0, 0);
-            Size = UDim2.new(0.75, -4, 0, 20);
+            Size = UDim2.new(1, -1, 0, 15);
             ZIndex = 5;
             Parent = Container;
         });
